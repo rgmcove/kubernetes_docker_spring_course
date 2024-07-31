c115a3bfa377d39d05a10c5e104fc3944a9af15c
Rubén Gómez Mosquera <ralberto115@gmail.com>
2024-07-30 22:38:48 -0500

    Creando y configurando servicio Spring cloud gateway

---
+++ msvc-gateway/src/test/java/org/rgomez/springcloud/msvc/msvc_gateway/MsvcGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.rgomez.springcloud.msvc.msvc_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
