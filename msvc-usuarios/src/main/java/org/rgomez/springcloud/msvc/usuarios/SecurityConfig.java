8090be1ea880289cd7ee47b25a9fc0d967404bcb
Rubén Gómez Mosquera <ralberto115@gmail.com>
2024-08-06 21:55:58 -0500

    Configurando OAuth2 Resource server en msvc usuarios

--- msvc-usuarios/src/main/java/org/rgomez/springcloud/msvc/usuarios/SecurityConfig.java
+++ msvc-usuarios/src/main/java/org/rgomez/springcloud/msvc/usuarios/SecurityConfig.java
@@ -1,6 +1,7 @@
 package org.rgomez.springcloud.msvc.usuarios;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -13,10 +14,16 @@ public class SecurityConfig {
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
+        http.authorizeHttpRequests(authorize -> authorize.requestMatchers("/authorized").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/").hasAnyAuthority("SCOPE_read", "SCOPE_write")
+                        .requestMatchers(HttpMethod.POST, "/").hasAuthority("SCOPE_write")
+                        .requestMatchers(HttpMethod.PUT, "/{id}").hasAuthority("SCOPE_write")
+                        .requestMatchers(HttpMethod.DELETE, "/{id}").hasAuthority("SCOPE_write")
+                        .anyRequest().authenticated())
                 .sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .oauth2Login(oau2Login -> oau2Login.loginPage("/oauth2/authorization/msvc-usuarios-client"))
-                .oauth2Login(withDefaults());
+                .oauth2Login(withDefaults())
+                .oauth2ResourceServer().jwt();
 
         return http.build();
     }
